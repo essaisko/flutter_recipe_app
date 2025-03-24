0edbdf4725063ef405117415affc7937c54a231e
essaisko <essaisko@gmail.com>
2025-03-24 19:09:43 +0900

    Add additional project files and prefab configuration for multiple architectures

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_recipe_app/recipe_list_item.dart';
 import 'package:flutter_recipe_app/recipe_menu.dart';
@@ -13,7 +14,12 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'NotoSanasKR',
+      ),
+      home: RecipePage(),
+    );
   }
 }
 
@@ -21,16 +27,36 @@ class RecipePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          RecipeTitle(),
-          RecipeMenu(),
-          RecipeListItem(),
-          RecipeListItem(),
-          RecipeListItem(),
+      appBar: AppBar(
+        actions: [
+          Icon(
+            CupertinoIcons.search,
+            size: 30,
+          ),
+          SizedBox(
+            width: 15,
+          ),
+          Icon(
+            CupertinoIcons.heart,
+            size: 30,
+            color: Colors.red,
+          ),
+          SizedBox(
+            width: 15,
+          )
         ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: ListView(
+          children: [
+            RecipeTitle(),
+            RecipeMenu(),
+            RecipeListItem('assets/images/coffee.jpg', 'Made Coffee'),
+            RecipeListItem('assets/images/burger.jpg', 'Burger'),
+            RecipeListItem('assets/images/pizza.jpg', 'Pizza'),
+          ],
+        ),
       ),
     );
   }
--- lib/recipe_list_item.dart
+++ lib/recipe_list_item.dart
@@ -1,8 +1,48 @@
 import 'package:flutter/material.dart';
 
 class RecipeListItem extends StatelessWidget {
+  final String path;
+  final String title;
+
+  RecipeListItem(this.path, this.title, {Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return Text('ReciepeListItem');
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          AspectRatio(
+            aspectRatio: 2 / 1,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset(
+                path,
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Text(
+            "Made $title",
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ),
+          SizedBox(
+            height: 4,
+          ),
+          Text(
+            "Have you ever made your own $title?",
+            style: TextStyle(
+              fontSize: 12,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
